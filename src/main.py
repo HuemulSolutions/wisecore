4e78d7223d7b67d5949a6e1d331b80bac452208d
matibarrivera <matias.barrera@huemulsolutions.com>
2025-08-11 11:19:00 -0400

    Agregar middleware ProxyHeaders para manejar encabezados de proxy en la configuración de FastAPI

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
 from src.routes.generation_routes import router as generation_router
 from src.routes.execution_routes import router as execution_router
 from src.routes.document_routes import router as document_router
@@ -10,6 +11,8 @@ from src.routes.organization_routes import router as organization_router
 app = FastAPI()
 
 # Configuración CORS
+app.add_middleware(ProxyHeadersMiddleware, trusted_hosts="*")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -17,6 +20,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 # Include the generation router
 app.include_router(generation_router)
